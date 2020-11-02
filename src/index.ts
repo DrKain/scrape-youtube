8b27509679d73b349269446ff2b77a3fb9e9b820
Kain <drkain@outlook.com>
2020-11-02 15:55:13 +1300

    1.1.0
    - Add debugging option
    - Add support for movies
    - Add support for playlists
    - Add support for live streams
    - Modify results data
    - Fix invalid types in url creation

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
+import { ResultFilter, ResultType, SearchOptions, Video, Playlist, Results, LiveStream } from './interface';
+import { getStreamData, getPlaylistData, getVideoData } from './parser';
 import { get } from 'https';
-import { ResultFilter, ResultType, SearchOptions, Video } from './interface';
-import { getVideoData } from './parser';
 
 class Youtube {
+    public debug = false;
     constructor() { }
 
     private getURL(query: string, options: SearchOptions): string {
         const url = new URL('/results', 'https://www.youtube.com');
-        url.search = new URLSearchParams({
-            search_query: query,
-            sp: ResultFilter[(options.type || 'video') as ResultType],
-            page: `${options.page ?? 0}`
-        }).toString();
-        return url.href;
+        url.search = new URLSearchParams({ search_query: query }).toString();
+        return url.href + '&sp=' + ResultFilter[(options.type || 'video') as ResultType];
     }
 
     private extractRenderData(page: string): Promise<JSON> {
@@ -34,7 +31,7 @@ class Youtube {
                         .contents
                 );
             } catch (e) {
-                console.log(e);
+                if (this.debug) console.log(e);
                 reject('Failed to extract video data. The request may have been blocked');
             }
         });
@@ -44,24 +41,48 @@ class Youtube {
      * Parse the data extracted from the page to match each interface
      * @param data Video Renderer Data
      */
-    private parseData(data: any): Promise<Video[]> {
+    private parseData(data: any): Promise<Results> {
         return new Promise((resolve, reject) => {
             try {
-                const results: Video[] = [];
+                const results: Results = {
+                    videos: [],
+                    playlists: [],
+                    streams: []
+                };
 
                 data.forEach((item: any) => {
-                    const vRender = item['videoRenderer'];
-                    if (!vRender) return;
 
-                    try {
-                        const result: Video = getVideoData(vRender);
-                        results.push(result);
-                    } catch (e) {
-                        console.log(e);
+                    if (item['videoRenderer'] && item['videoRenderer']['lengthText']) {
+                        try {
+                            const result: Video = getVideoData(item['videoRenderer']);
+                            results.videos.push(result);
+                        } catch (e) {
+                            if (this.debug) console.log(e);
+                        }
                     }
 
-                    resolve(results);
+
+                    if (item['videoRenderer'] && !item['videoRenderer']['lengthText']) {
+                        try {
+                            const result: LiveStream = getStreamData(item['videoRenderer']);
+                            results.streams.push(result);
+                        } catch (e) {
+                            if (this.debug) console.log(e);
+                        }
+                    }
+
+
+                    if (item['playlistRenderer']) {
+                        try {
+                            const result: Playlist = getPlaylistData(item['playlistRenderer']);
+                            results.playlists.push(result);
+                        } catch (e) {
+                            if (this.debug) console.log(e);
+                        }
+                    }
                 });
+
+                resolve(results);
             } catch (e) {
                 console.warn(e);
                 reject('Fatal error when parsing result data. Please report this on GitHub');
@@ -76,6 +97,7 @@ class Youtube {
      */
     private load(query: string, options: SearchOptions): Promise<string> {
         const url = this.getURL(query, options);
+        if (this.debug) console.log(url);
 
         return new Promise((resolve, reject) => {
             get(url, res => {
@@ -87,17 +109,11 @@ class Youtube {
         });
     }
 
-    /**
-     * Search YouTube for a list of videos
-     * @param query Search Query
-     * @param options Optional Search Options
-     */
-    public search(query: string, options: SearchOptions = {}): Promise<Video[]> {
+    public search(query: string, options: SearchOptions = {}): Promise<Results> {
         return new Promise(async (resolve, reject) => {
             try {
-                const page = await this.load(query, options);
+                const page = await this.load(query, options || {});
                 const data = await this.extractRenderData(page);
-                require('fs').writeFileSync('debug.json', JSON.stringify(data, null, 2));
                 const results = await this.parseData(data);
                 resolve(results);
             } catch (e) {
